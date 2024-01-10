0c1be428b8cd9b8dccbd8f01dd037c89bbbdd7b7
Edwin Gichuhi <edwingichuhis@gmail.com>
2024-01-10 17:59:07 +0300

    Add createPost method to FeedController

--- src/feed/services/feed.service.ts
+++ src/feed/services/feed.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { FeedPostEntity } from '../models/post.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { FeedPost } from '../models/post.interface';
 
 @Injectable()
-export class FeedService {}
+export class FeedService {
+  constructor(
+    @InjectRepository(FeedPostEntity)
+    private readonly feedPostRepository: Repository<FeedPostEntity>,
+  ) {}
+
+  createPost(feedPost: FeedPost) {
+    return this.feedPostRepository.save(feedPost);
+  }
+}
