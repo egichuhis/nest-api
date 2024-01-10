55633eca85074c3221468f6edc1cd68416052d73
Edwin Gichuhi <edwingichuhis@gmail.com>
2024-01-10 19:47:46 +0300

    Add GET and PUT methods to FeedController

--- src/feed/controllers/feed.controller.ts
+++ src/feed/controllers/feed.controller.ts
@@ -1,13 +1,27 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { FeedService } from '../services/feed.service';
 import { FeedPost } from '../models/post.interface';
 import { Observable } from 'rxjs';
+import { UpdateResult } from 'typeorm';
 
 @Controller('feed')
 export class FeedController {
   constructor(private feedService: FeedService) {}
   @Post()
-  create(@Body() post: FeedPost): Observable<FeedPost> {
-    return this.feedService.createPost(post);
+  create(@Body() feedPost: FeedPost): Observable<FeedPost> {
+    return this.feedService.createPost(feedPost);
+  }
+
+  @Get()
+  findAll(): Observable<FeedPost[]> {
+    return this.feedService.findAllPosts();
+  }
+
+  @Put(':id')
+  update(
+    @Param('id') id: number,
+    @Body() feedPost: FeedPost,
+  ): Observable<UpdateResult> {
+    return this.feedService.updatePost(id, feedPost);
   }
 }
